8137f7eebec9b9c1ba5cb0c23b5db8cabe751947
michgur <michael@thegur.net>
2023-01-19 17:36:17 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import itertools
+import oneai
+import csv
+
+
+def upload_csv_to_collection(
+    source_file_path: str,
+    collection_name: str,
+    *,
+    main_column: str = None,  # column 0 if None
+    timestamp_column: str = None,  # ignored if None
+    skills: list[str] = None,
+    input_skill: str = None,
+    row_range_start: int = 0,
+    row_range_end: int = None,
+):
+    if input_skill is not None and input_skill not in skills:
+        raise ValueError("input_skill must be in skills")
+
+    pipeline = oneai.Pipeline(
+        skills.map(oneai.Skill.__new__)
+        + [oneai.skills.Clustering(collection=collection_name, input_skill=input_skill)]
+    )
+
+    with open(source_file_path, "r", newline="") as inputf:
+        reader = csv.reader(inputf)
+        header = next(reader)
+        indices = list(range(len(header)))
+        if main_column is None:
+            main_column = 0
+        elif isinstance(main_column, "str"):
+            main_column = header.index(main_column)
+            if main_column == -1:
+                raise ValueError("main_column not found in header")
+        if isinstance(timestamp_column, "str"):
+            timestamp_column = header.index(timestamp_column)
+            if timestamp_column == -1:
+                raise ValueError("timestamp_column not found in header")
+
+        pipeline.run_batch(
+            oneai.Input(
+                row[main_column],
+                timestamp=row[timestamp_column]
+                if timestamp_column is not None
+                else None,
+                metadata={
+                    k: v
+                    for (k, v, i) in zip(header, row, indices)
+                    if i not in (main_column, timestamp_column)
+                },
+            )
+            for row in itertools.islice(
+                reader, max(row_range_start - 1, 0), row_range_end
+            )
+        )
