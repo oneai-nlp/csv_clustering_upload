1917e8fd5f4d70aa9f4ce129f07a3e347d1d1ba3
michgur <michael@thegur.net>
2023-01-19 17:51:40 +0200

    filename

--- clustering_main.py
+++ clustering_main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import itertools
 import oneai
 import csv
@@ -9,38 +10,40 @@ def upload_csv_to_collection(
     *,
     main_column: str = None,  # column 0 if None
     timestamp_column: str = None,  # ignored if None
+    timestamp_format: str = "%a %b %d %H:%M:%S %Y",
     skills: list[str] = None,
     input_skill: str = None,
     row_range_start: int = 0,
     row_range_end: int = None,
+    encoding: str = "utf-8",
 ):
     if input_skill is not None and input_skill not in skills:
         raise ValueError("input_skill must be in skills")
 
     pipeline = oneai.Pipeline(
-        skills.map(oneai.Skill.__new__)
+        list(skills.map(oneai.Skill.__new__) if skills else [])
         + [oneai.skills.Clustering(collection=collection_name, input_skill=input_skill)]
     )
 
-    with open(source_file_path, "r", newline="") as inputf:
+    with open(source_file_path, "r", newline="", encoding=encoding) as inputf:
         reader = csv.reader(inputf)
         header = next(reader)
         indices = list(range(len(header)))
         if main_column is None:
             main_column = 0
-        elif isinstance(main_column, "str"):
+        elif isinstance(main_column, str):
             main_column = header.index(main_column)
             if main_column == -1:
                 raise ValueError("main_column not found in header")
-        if isinstance(timestamp_column, "str"):
+        if isinstance(timestamp_column, str):
             timestamp_column = header.index(timestamp_column)
             if timestamp_column == -1:
                 raise ValueError("timestamp_column not found in header")
 
-        pipeline.run_batch(
+        batch = [
             oneai.Input(
                 row[main_column],
-                timestamp=row[timestamp_column]
+                timestamp=datetime.strptime(row[timestamp_column], timestamp_format)
                 if timestamp_column is not None
                 else None,
                 metadata={
@@ -52,4 +55,15 @@ def upload_csv_to_collection(
             for row in itertools.islice(
                 reader, max(row_range_start - 1, 0), row_range_end
             )
-        )
+        ]
+        pipeline.run_batch(batch)
+
+
+oneai.api_key = "9595c022-b1f7-496e-a215-c5ee0ff9d65d"
+oneai.URL = "https://staging.oneai.com"
+oneai.multilingual = True
+oneai.MAX_CONCURRENT_REQUESTS = 5
+
+upload_csv_to_collection(
+    "./test.csv", "csv_upload_test_v1", row_range_end=10, encoding="ISO-8859-1"
+)
